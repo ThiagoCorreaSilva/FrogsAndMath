a101aaf92315dbe85834e77752dce53626acf696
MushcV2 <titi.c2006@gmail.com>
2024-05-16 09:05:46 -0300

    InventorySystem 1.0

--- Assets/MyProject/Scripts/Player/Inventory.cs
+++ Assets/MyProject/Scripts/Player/Inventory.cs
@@ -5,21 +5,40 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField] private Button inventoryButton;
+    [SerializeField] private Button openInventory;
+    [SerializeField] private Button closeInventory;
     [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private Button[] slots;
+    [SerializeField] private bool[] inventoryOccupied;
     private Player player;
 
     private void Start()
     {
+        openInventory.onClick.AddListener(OpenInventory);
+        closeInventory.onClick.AddListener(CloseInventory);
+
         inventoryPanel.SetActive(false);
+        closeInventory.gameObject.SetActive(false);
 
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-
-        inventoryButton.onClick.AddListener(OpenInventory);
     }
 
     private void OpenInventory()
     {
         inventoryPanel.SetActive(true);
+        openInventory.gameObject.SetActive(false);
+        closeInventory.gameObject.SetActive(true);
+    }
+
+    private void CloseInventory()
+    {
+        inventoryPanel.SetActive(false);
+        openInventory.gameObject.SetActive(true);
+        closeInventory.gameObject.SetActive(false);
+    }
+
+    private void UseItem()
+    {
+        Debug.Log("Usou item");
     }
 }
