baf81841338de7813cd9ade479f8efcba50d6c67
MushcV2 <titi.c2006@gmail.com>
2024-05-16 09:40:51 -0300

    InventorySystem 3.0

--- Assets/MyProject/Scripts/Items/Items.cs
+++ Assets/MyProject/Scripts/Items/Items.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Items : MonoBehaviour
 {
     [SerializeField] protected float value;
     [SerializeField] protected bool isUsable;
-    public Sprite sprite;
+    private Image itemSprite;
     private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        itemSprite = GetComponent<Image>();
+    }
 
     protected void Effect()
     {
+        if (!isUsable) return;
+
         for (int i = 0; i < inventory.inventoryOccupied.Length; i++)
         {
             if (inventory.inventoryOccupied[i] == false)
             {
                 inventory.inventoryOccupied[i] = true;
+                inventory.slots[i].image = itemSprite;
+                gameObject.SetActive(false);
+
                 Debug.Log("Deu certo");
+                return;
             }
             else
             {
