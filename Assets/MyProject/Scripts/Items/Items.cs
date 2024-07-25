d1c4be254f341fbb52f87545f58394adcda3bc6a
MushcV2 <142312690+MushcV2@users.noreply.github.com>
2024-07-25 16:23:51 -0300

    HealPot Rework

--- Assets/MyProject/Scripts/Items/Items.cs
+++ Assets/MyProject/Scripts/Items/Items.cs
@@ -10,10 +10,28 @@ public class Items : MonoBehaviour
     [SerializeField] protected Player player;
     private Inventory inventory;
 
+    [Header("Up and Down")]
+    [SerializeField] protected float amplitude;
+    [SerializeField] protected float frequency;
+    [SerializeField] protected bool haveEffect;
+    protected Vector2 initialPos;
+
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        initialPos = transform.position;
+    }
+
+    protected virtual void Update()
+    {
+        if (haveEffect)
+        {
+            Vector3 _newPos = initialPos;
+            _newPos.y += Mathf.Sin(Time.timeSinceLevelLoad * Mathf.PI * frequency) * amplitude;
+            transform.position = _newPos;
+        }
     }
 
     public virtual void Effect()
